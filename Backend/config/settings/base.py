10257cdeed9108ad4009b5117e610f56c20d6422
shamil-anfas <shamilanfas.in@gmail.com>
2026-08-19 20:29:00 +0530

    Update base.py

--- Backend/config/settings/base.py
+++ Backend/config/settings/base.py
@@ -242,7 +242,7 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://127.0.0.1:6379/0")
+CELERY_BROKER_URL = env("CELERY_BROKER_URL")
 
 
 # ---------------------------------------------------------------------------
